2ac6cfdd999b02d31145949b34a87d3729992c8e
unknown <s.umekawa.sys21@morijyobi.ac.jp>
2022-03-23 10:09:18 +0900

    正解

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ public class Main{
     public static void Main(String []args){
         Scanner ensyu =new Scanner(System.in);
         double sin,tai,bmi;
+        for(int i=0; i<=1; i++){
         sin=ensyu.nextDouble();
         tai=ensyu.nextDouble();
         bmi=tai/sin/sin;
         bmi=((double)Math.round(bmi * 10))/10;
         System.out.println(bmi);
-
+        }
     }
 
 }
